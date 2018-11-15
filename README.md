09f31e7f96e7191e11d78b30408f485b87b49dfd
Axel Stenson <stens103@umn.edu>
2018-11-15 13:45:20 -0600

    uploading

---
+++ README.md
@@ -0,0 +1,27 @@
+
+
+<p align="center">
+  <img src ="src/snoo.128png">
+</p>
+
+#Karma-Counter
+
+A reddit karma counter that displays karma differently depending on number to
+fit within Chrome's badge char limit constraints.
+
+## Behavior
+
+Clicking the counter will switch from displaying comment karma, which
+is the default, to link karma. Clicking again will switch back.
+
+## Installation
+
+Gloom can be installed through the Chrome extension manager by searching for "karma kounter"
+
+## Credit
+
+Karma Kounter is coded, optimized, and maintained by Axel Stenson.
+
+## License
+
+Licensed under [MIT License](https://github.com/axellarsstenson/karma-kounter/blob/master/LICENSE.md) and open source.
---
+++ background.js
@@ -0,0 +1,67 @@
+var fetchReddit = function() {
+  // Setup and open API
+  var xhr = new XMLHttpRequest();
+  xhr.open("GET", "https://www.reddit.com/api/me.json", true);
+  xhr.onreadystatechange = function() {
+
+    if (xhr.readyState == 4) {
+      // Parse text for use
+      var redditdata = JSON.parse(xhr.responseText);
+      if (redditdata.data.comment_karma && redditdata.data.link_karma) {
+
+        // Check if user wants to use Comment or Link Karma
+        if (displaySetting == true){
+            usekarma = redditdata.data.comment_karma;
+        }
+        else {
+            usekarma = redditdata.data.link_karma;
+        }
+
+
+        // Display changes depending on Karma number
+        if (usekarma <= 999){
+          // Show karma as is
+          chrome.browserAction.setBadgeText({ text: usekarma + "" });
+        }
+        else if (usekarma > 999 && usekarma < 10000){
+          // Show x.yk karma where x is some thousand and y is some hundred.
+          chrome.browserAction.setBadgeText({ text: usekarma =
+            ((usekarma - (usekarma % 100)) / 1000) + "k" });
+        }
+        else if (usekarma >= 10000 && usekarma < 1000000){
+          // Show xk where x is some number of thousands.
+          chrome.browserAction.setBadgeText({ text:
+            ((usekarma - (usekarma % 1000)) / 1000) + "k" });
+        }
+        else {
+          // Show x.ym where x is some million and y is some hundred thousand.
+          chrome.browserAction.setBadgeText({ text: usekarma =
+            ((usekarma - (usekarma % 100000)) / 1000000) + "m" });
+        }
+
+      } else {
+        // Issue with connection
+        chrome.browserAction.setBadgeText("???")
+      }
+
+    } else {
+      // Loading 'icon'
+      chrome.browserAction.setBadgeText({ text: "~" });
+    }
+  };
+  xhr.send();
+};
+
+// Load number when new page is opened.
+chrome.browserAction.setBadgeBackgroundColor({color: '#276795'})
+var displaySetting = true;
+fetchReddit();
+
+// for testing 5 seconds.
+setInterval(fetchReddit, 30000);
+
+chrome.browserAction.onClicked.addListener(function(tab) {
+  displaySetting = !displaySetting;
+  fetchReddit();
+  // chrome.browserAction.setBadgeText({ text: karma });
+});
