f28775461e461e340aa60c8f511539a0076c0e5e
Axel Stenson <axellarsstenson@gmail.com>
2018-11-16 16:33:13 -0600

    adding comment info on background

--- background.js
+++ background.js
@@ -1,3 +1,8 @@
+// Copyright 2017 Axel Stenson. All rights reserved.
+// Use of this source code is governed by a MIT-style license that can be
+// found in the LICENSE file.
+
+
 var checkKarma = function() {
   // Setup and open API
   var xhr = new XMLHttpRequest();
